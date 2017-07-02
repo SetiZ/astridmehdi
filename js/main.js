a65430cf44dce78c7e74d89fc0ac8787f42d581e
SetiZ <funkysetiz@gmail.com>
2017-07-02 17:27:49 +0200

    design

--- js/main.js
+++ js/main.js
@@ -87,7 +87,7 @@
 								$header.css('padding', '');
 							else {
 
-								var p = Math.max(192, ($window.height() - $header_header.outerHeight()) / 2);
+								var p = Math.max(47, ($window.height() - $header_header.outerHeight()) / 2);
 								$header.css('padding', p + 'px 0 ' + p + 'px 0');
 
 							}
